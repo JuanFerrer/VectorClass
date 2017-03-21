7173a2b1c0986db16160fbdac9ffd04ee8a263a7
JuanFerrer <juanemilioferrer@outlook.com>
2017-03-21 18:44:35 +0000

    Update
    Latest version

--- VectorClass.cpp
+++ VectorClass.cpp
@@ -1,323 +1,409 @@
 //Juan Emilio Ferrer
 //Vector class
 
-#include ".\VectorClass.h"
+#include "VectorClass.h"
 
+#include <ostream>
+
+//========//
 ///Vector2//
-//Default constructor. Origin
+//========//
+
+// Default constructor. Origin
 vector2::vector2()
 {
-	x = 0.0f;
-	y = 0.0f;
-	length = 0.0f;
+	mX = 0.0f;
+	mY = 0.0f;
+	mLength = 0.0f;
 }
 //
 vector2::vector2(float X, float Y)
 {
-	x = X;
-	y = Y;
+	mX = X;
+	mY = Y;
 	UpdateLength();
 }
-//Copy constructor
+// Copy constructor
 vector2::vector2(const vector2& other)
 {
-	x = other.x;
-	y = other.y;
+	mX = other.mX;
+	mY = other.mY;
 	UpdateLength();
 }
-//Destructor
-vector2::~vector2()
+
+// Accessors
+float vector2::GetX() const { return mX; }
+float vector2::GetY() const { return mY; }
+float vector2::GetLength() const { return mLength; }
+// Mutators
+void vector2::SetX(const float& newValue)
 {
-}
-//Accessors
-float vector2::GetX() const
-{
-	return x;
-}
-float vector2::GetY() const
-{
-	return y;
-}
-float vector2::GetLength() const
-{
-	return length;
-}
-//Mutators
-void vector2::SetX(float newValue)
-{
-	if (x != newValue)
+	if (mX != newValue)
 	{
-		x = newValue;
+		mX = newValue;
 		UpdateLength();
 	}
 }
-void vector2::SetY(float newValue)
+void vector2::SetY(const float& newValue)
 {
-	if (y != newValue)
+	if (mY != newValue)
 	{
-		y = newValue;
+		mY = newValue;
 		UpdateLength();
 	}
 }
-void vector2::IncreaseX(float increaseAmount)
-{
-	x += increaseAmount;
-	UpdateLength();
-}
-void vector2::IncreaseY(float increaseAmount)
-{
-	y += increaseAmount;
-	UpdateLength();
-}
-void vector2::Set(const vector2& other)
-{
-	x = other.x;
-	y = other.y;
-	UpdateLength();
-}
-//Copy assignment
+// Copy assignment
 vector2& vector2::operator= (const vector2& rhs)
 {
 	if (this != &rhs)
 	{
-		x = rhs.x;
-		y = rhs.y;
+		mX = rhs.mX;
+		mY = rhs.mY;
 		UpdateLength();
 	}
 	return *this;
 }
-//Operators
+// Operators
 vector2 vector2::operator+ (const vector2& rhs) const
 {
-	return vector2 (x + rhs.x, y + rhs.y);
+	return vector2 (mX + rhs.mX, mY + rhs.mY);
 }
 vector2 vector2::operator- (const vector2& rhs) const
 {
-	return vector2(x - rhs.x, y - rhs.y);
+	return vector2(mX - rhs.mX, mY - rhs.mY);
 }
 vector2& vector2::operator+= (const vector2& rhs)
 {
-	this->x += rhs.x;
-	this->y += rhs.y;
+	this->mX += rhs.mX;
+	this->mY += rhs.mY;
 	UpdateLength();
 	return *this;
 }
 vector2& vector2::operator -= (const vector2& rhs)
 {
-	this->x -= rhs.x;
-	this->y -= rhs.y;
+	this->mX -= rhs.mX;
+	this->mY -= rhs.mY;
 	UpdateLength();
 	return *this;
 }
-
-//To use with std::cout
+vector2 vector2::operator*(const float &rhs) const
+{
+	return vector2(mX * rhs, mY * rhs);
+}
+vector2& vector2::operator*=(const float &rhs)
+{
+	this->mX *= rhs;
+	this->mY *= rhs;
+	UpdateLength();
+	return *this;
+}
+bool vector2::operator==(const vector2 & rhs) const
+{
+	return (this->mX == rhs.mX) && (this->mY == rhs.mY);
+}
+bool vector2::operator!=(const vector2 & rhs) const
+{
+	return !(*this == rhs);
+}
+// To use with std::cout
 std::ostream& operator<< (std::ostream& o, const vector2& rhs)
 {
 	return o << '(' << rhs.GetX() << ',' << rhs.GetY() << ')';
 }
 
-//Called when coordinates are modified. Sets length to new value
+// Called when coordinates are modified. Sets length to new value
 void vector2::UpdateLength()
 {
 	//Calculate length
-	float sum = (x * x) + (y * y);
-	if (sum > 0.0f)
-	{
-		length = sqrt(sum);
-	}
-	else
-	{
-		length = 0.0f;
-	}
+	float sum = (mX * mX) + (mY * mY);
+	mLength = sum > 0.0f ? sqrt(sum) : 0.0f;
 }
-//Modify coordinates to make vector length 1
+// Modify coordinates to make vector length 1
 void vector2::Normalise()
 {
-	if (length > 0.0f)
+	if (mLength > 0.0f)
 	{
-		x /= length;
-		y /= length;
+		mX /= mLength;
+		mY /= mLength;
 		UpdateLength();
 	}
 	else		//Invalid vector?
 	{
-		x = 0.0f;
-		y = 0.0f;
+		mX = 0.0f;
+		mY = 0.0f;
 	}
 }
-//Set coordinates to origin
+vector2 vector2::GetNormalised()
+{
+	vector2 nV = *this;
+	nV.Normalise();
+	return nV;
+}
+// Rotate vector. Takes radians
+void vector2::Rotate(float rotationAngle)
+{
+	float cosA = cos(rotationAngle);
+	float sinA = sin(rotationAngle);
+	float tempX = mX;
+	mX = mX * cosA - mY * sinA;
+	mY = tempX * sinA + mY * cosA;
+	UpdateLength();
+}
+// Dot product
+float vector2::Dot(const vector2& other)
+{
+	return acos(mX * other.GetX() + mY * other.GetY());
+}
+// Angle in rad to vector in the range [0, PI] by default. If absolute, angle in the range [-PI, PI]
+float vector2::AngleTo(vector2& other, bool absolute)
+{
+	vector2 v(this->GetNormalised());
+	vector2 rightOfV(v.GetY(), -v.GetX());	// Vector to the right of V
+	vector2 w(other.GetNormalised());
+	float angle = acos(((v.GetX() * w.GetX()) + (v.GetY() * w.GetY())));
+	if (absolute)
+	{
+		if (w.Dot(rightOfV) >= 0.0f)
+			return angle;
+		else return -angle;
+	}
+	return angle;
+}
+// Set coordinates to origin
 void vector2::Reset()
 {
-	x = 0.0f;
-	y = 0.0f;
-	length = 0.0f;
+	mX = 0.0f;
+	mY = 0.0f;
+	mLength = 0.0f;
 }
 
 
 
-
+//========//
 ///Vector3//
+//========//
+
+//Z up...Fixing?
+
+// Default constructor. Origin
 vector3::vector3()
 {
-	x = 0.0f;
-	y = 0.0f;
-	z = 0.0f;
-	length = 0.0f;
+	mX = 0.0f;
+	mY = 0.0f;
+	mZ = 0.0f;
+	mLength = 0.0f;
 }
 //
 vector3::vector3(float X, float Y, float Z)
 {
-	x = X;
-	y = Y;
-	z = Z;
+	mX = X;
+	mY = Y;
+	mZ = Z;
 	UpdateLength();
 }
-//Copy constructor
+// Copy constructor
 vector3::vector3(const vector3& other)
 {
-	x = other.x;
-	y = other.y;
-	z = other.z;
+	mX = other.mX;
+	mY = other.mY;
+	mZ = other.mZ;
 	UpdateLength();
 }
-//Destructor
-vector3::~vector3()
+// Accessors
+float vector3::GetX() const { return mX; }
+float vector3::GetY() const { return mY; }
+float vector3::GetZ() const { return mZ; }
+float vector3::GetLength() const { return mLength; }
+// Mutators
+void vector3::SetX(const float& newValue)
 {
+	if (mX != newValue)
+	{
+		mX = newValue;
+		UpdateLength();
+	}
 }
-//Accessors
-float vector3::GetX() const
+void vector3::SetY(const float& newValue)
 {
-	return x;
+	if (mY != newValue)
+	{
+		mY = newValue;
+		UpdateLength();
+	}
 }
-float vector3::GetY() const 
+void vector3::SetZ(const float& newValue)
 {
-	return y;
+	if (mZ != newValue)
+	{
+		mZ = newValue;
+		UpdateLength();
+	}
 }
-float vector3::GetZ() const
-{
-	return z;
-}
-float vector3::GetLength() const
-{
-	return length;
-}
-//Mutators
-void vector3::SetX(float newValue)
-{
-	x = newValue;
-	UpdateLength();
-}
-void vector3::SetY(float newValue)
-{
-	y = newValue;
-	UpdateLength();
-}
-void vector3::SetZ(float newValue)
-{
-	z = newValue;
-	UpdateLength();
-}
-void vector3::IncreaseX(float increaseAmount)
-{
-	x += increaseAmount;
-	UpdateLength();
-}
-void vector3::IncreaseY(float increaseAmount)
-{
-	y += increaseAmount;
-	UpdateLength();
-}
-void vector3::IncreaseZ(float increaseAmount)
-{
-	z += increaseAmount;
-	UpdateLength();
-}
-void vector3::Set(const vector3 &other)
-{
-	x = other.x;
-	y = other.y;
-	z = other.z;
-	UpdateLength();
-}
-//Copy assignment
+// Copy assignment
 vector3& vector3::operator= (const vector3& rhs)
 {
 	if (this != &rhs)
 	{
-		x = rhs.x;
-		y = rhs.y;
-		z = rhs.z;
-		length = rhs.length;
+		mX = rhs.mX;
+		mY = rhs.mY;
+		mZ = rhs.mZ;
+		UpdateLength();
 	}
 	return *this;
 }
-//Operators
+// Operators
 vector3 vector3::operator+ (const vector3& rhs) const
 {
-	return vector3(x + rhs.x, y + rhs.y, z + rhs.z);
+	return vector3(mX + rhs.mX, mY + rhs.mY, mZ + rhs.mZ);
 }
 vector3 vector3::operator- (const vector3& rhs) const
 {
-	return vector3(x - rhs.x, y - rhs.y, z - rhs.z);
+	return vector3(mX - rhs.mX, mY - rhs.mY, mZ - rhs.mZ);
 }
 vector3& vector3::operator+= (const vector3& rhs)
 {
-	this->x += rhs.x;
-	this->y += rhs.y;
-	this->z += rhs.z;
+	this->mX += rhs.mX;
+	this->mY += rhs.mY;
+	this->mZ += rhs.mZ;
 	UpdateLength();
 	return *this;
 }
 vector3& vector3::operator-= (const vector3& rhs)
 {
-	this->x -= rhs.x;
-	this->y -= rhs.y;
-	this->z -= rhs.z;
+	this->mX -= rhs.mX;
+	this->mY -= rhs.mY;
+	this->mZ -= rhs.mZ;
 	UpdateLength();
 	return *this;
 }
-
-//To use with std::cout
+vector3 vector3::operator*(const float& rhs) const
+{
+	return vector3(mX * rhs, mY * rhs, mZ * rhs);
+}
+vector3 & vector3::operator*=(const float& rhs)
+{
+	this->mX *= rhs;
+	this->mY *= rhs;
+	this->mZ *= rhs;
+	UpdateLength();
+	return *this;
+}
+// To use with std::cout
 std::ostream& operator<< (std::ostream& o, const vector3& rhs)
 {
 	return o << '(' << rhs.GetX() << ',' << rhs.GetY() << ',' << rhs.GetZ() << ')';
 }
-//Called when coordinates are modified. Sets length to new value
+// Called when coordinates are modified. Sets length to new value
 void vector3::UpdateLength()
 {
 	//Calculate length
-	float sum = (x * x) + (y * y) + (z * z);
-	if (sum > 0.0f)
-	{
-		length = sqrt(sum);
-	}
-	else		//Invalid vector?
-	{
-		length = 0.0f;
-	}
+	float sum = (mX * mX) + (mY * mY) + (mZ * mZ);
+	mLength = sum > 0.0f ? sqrt(sum) : 0.0f;
 }
-//Modify coordinates to make vector length 1
+// Modify coordinates to make vector length 1
 void vector3::Normalise()
 {
-	if (length > 0.0f)
+	if (mLength > 0.0f)
 	{
-		x /= length;
-		y /= length;
-		z /= length;
+		mX /= mLength;
+		mY /= mLength;
+		mZ /= mLength;
 		UpdateLength();
 	}
 	else		//Invalid vector?
 	{
-		x = 0.0f;
-		y = 0.0f;
-		z = 0.0f;
+		mX = 0.0f;
+		mY = 0.0f;
+		mZ = 0.0f;
 	}
 }
-//Set coordinates to origin
+vector3 vector3::GetNormalised()
+{
+	vector3 nV = *this;
+	nV.Normalise();
+	return nV;
+}
+
+
+//       !!!
+//      !!!!!
+//      !!!!!
+//      !!!!!
+//      !!!!!
+//      !!!!!
+//       !!!
+// 
+//       !!!
+//       !!!
+
+
+
+// Rotate vector in X axis. Takes radians. Left-hand rule, Y-up
+// Not working yet
+void vector3::RotateX(float rotationAngle)
+{
+	float cosA = cos(rotationAngle);
+	float sinA = sin(rotationAngle);
+
+	float tempY = mY;
+
+	mX = mX;
+	mY = (mY * cosA) - (mZ * sinA);
+	mZ = -(tempY * sinA) + (mZ * cosA);
+	UpdateLength();
+}
+// Rotate vector in Y axis. Takes radians. Left-hand rule, Y-up
+// Not working yet
+void vector3::RotateY(float rotationAngle)
+{
+	float cosA = cos(rotationAngle);
+	float sinA = sin(rotationAngle);
+
+	float tempX = mX;
+
+	mX = (mX * cosA) - (mZ * sinA);
+	mY = mY;
+	mZ = (tempX * sinA) + (mZ * cosA);
+	UpdateLength();
+}
+// Rotate vector in Z axis. Takes radians. Left-hand rule, Y-up
+// Not working yet
+void vector3::RotateZ(float rotationAngle)
+{
+	float cosA = cos(rotationAngle);
+	float sinA = sin(rotationAngle);
+
+	float tempX = mX;
+
+	mX = (mX * cosA) + (mY * sinA);
+	mY = (tempX * sinA) + (mY * cosA);
+	mZ = mZ;
+	UpdateLength();
+}
+
+// Angle in rad to passed vector
+float vector3::AngleTo(vector3& other)
+{
+	vector3 v(this->GetNormalised());
+	vector3 w(other.GetNormalised());
+	return fabs(acos((v.GetX() * w.GetX()) + (v.GetY() * w.GetY()) + (v.GetZ() * w.GetZ())));
+}
+// Dot product with another vector
+float vector3::Dot(const vector3& other)
+{
+	return acos(mX * other.GetX() + mY * other.GetY() + mZ*other.GetZ());
+}
+
+// Get orthogonal vector to this and other
+vector3 vector3::Cross(vector3& other)
+{
+	return vector3(mY * other.GetZ() - other.GetY() * mZ, mZ * other.GetX() - other.GetZ() * mX, mX * other.GetY() - other.GetX() * mY);
+}
+// Set coordinates to origin
 void vector3::Reset()
 {
-	x = 0.0f;
-	y = 0.0f;
-	z = 0.0f;
-	length = 0.0f;
+	mX = 0.0f;
+	mY = 0.0f;
+	mZ = 0.0f;
+	mLength = 0.0f;
 }
--- VectorClass.h
+++ VectorClass.h
@@ -10,96 +10,101 @@
 class vector2
 {
 private:
-	float x;				//X component of vector
-	float y;				//Y component of vector. Can be used as Z in 3D space
-	float length;			//Length of vector
+	float mX;				//X component of vector
+	float mY;				//Y component of vector. Can be used as Z in 3D space
+	float mLength;			//Length of vector
 
 	void UpdateLength();
 
 public:
-	//Constructors
+
+	// Constructors
 	vector2();
 	vector2(float X, float Y);
 	vector2(const vector2&);
 
-	//Destructor
-	~vector2();
-
-	//Accessors
+	// Accessors
 	float GetX() const;
 	float GetY() const;
 	float GetLength() const;
 
-	//Mutators
-	void SetX(float);
-	void SetY(float);
-	void IncreaseX(float);
-	void IncreaseY(float);
-	void Set(const vector2&);
-	//Length can not be set, only by adjusting the coordinates
+	// Mutators
+	void SetX(const float&);
+	void SetY(const float&);
+	// Length can not be set, only by adjusting the coordinates
 
-	//Copy Assignment
-	vector2& operator= (const vector2&);
+	// Copy Assignment
+	vector2& operator= (const vector2& rhs);
 
-	//Operators
-	vector2 operator+(const vector2&) const;
-	vector2 operator-(const vector2&) const;
-	vector2& operator+=(const vector2&);
-	vector2& operator-=(const vector2&);
+	// Operators
+	vector2 operator+(const vector2& rhs) const;
+	vector2 operator-(const vector2& rhs) const;
+	vector2& operator+=(const vector2& rhs);
+	vector2& operator-=(const vector2& rhs);
+	vector2 operator*(const float& rhs) const;
+	vector2& operator*=(const float& rhs);
+	bool operator==(const vector2& rhs) const;
+	bool operator!=(const vector2& rhs) const;
 	friend std::ostream& operator<< (std::ostream&, const vector2&);
 
-	//Member functions
+	// Member functions
 	void Normalise();
+	vector2 GetNormalised();
+	void Rotate(float);
+	float AngleTo(vector2& other, bool absolute = false);
+	float Dot(const vector2&);
 	void Reset();
 };
 
 class vector3
 {
 private:
-	float x;				//X component of vector
-	float y;				//Y component of vector
-	float z;				//Z component of vector
-	float length;			//Length of vector
+	float mX;				//X component of vector
+	float mY;				//Y component of vector
+	float mZ;				//Z component of vector
+	float mLength;			//Length of vector
 
 	void UpdateLength();
 
 public:
-	//Constructors
+	// Constructors
 	vector3();
 	vector3(float, float, float);
 	vector3(const vector3&);
 
-	//Destructor
-	~vector3();
-
-	//Accessors
+	// Accessors
 	float GetX() const;
 	float GetY() const;
 	float GetZ() const;
 	float GetLength() const;
 
-	//Mutators
-	void SetX(float);
-	void SetY(float);
-	void SetZ(float);
-	void IncreaseX(float);
-	void IncreaseY(float);
-	void IncreaseZ(float);
-	void Set(const vector3&);
-	//Length can not be set, only by adjusting the coordinates
+	// Mutators
+	void SetX(const float&);
+	void SetY(const float&);
+	void SetZ(const float&);
+	// Length can not be set, only by adjusting the coordinates
 
-	//Copy Assignment
+	// Copy Assignment
 	vector3& operator= (const vector3&);
 
-	//Operators
+	// Operators
 	vector3 operator+(const vector3&) const;
 	vector3 operator-(const vector3&) const;
 	vector3& operator+=(const vector3&);
 	vector3& operator-=(const vector3&);
+	vector3 operator*(const float&) const;
+	vector3& operator*=(const float&);
 	friend std::ostream& operator<< (std::ostream&, const vector3&);
 
-	//Member functions
+	// Member functions
 	void Normalise();
+	vector3 GetNormalised();
+	void RotateX(float);
+	void RotateY(float);
+	void RotateZ(float);
+	float AngleTo(vector3&);
+	float Dot(const vector3&);
+	vector3 Cross(vector3&);
 	void Reset();
 };
 
